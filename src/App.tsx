d085ca66d637fa57343c8b2bbb0b7a8eb83b41cb
Anthony Martini <anthonycm2022@gmail.com>
2025-03-31 22:25:46 -0400

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PlayingCard from "./assets/PlayingCard.jsx";
 import preflop from "./data/preflop.json";
 import "./index.css"; // Ensure Tailwind CSS is imported here
